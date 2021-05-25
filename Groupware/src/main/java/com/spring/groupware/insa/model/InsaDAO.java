a1fe0cb3d0c16045c3bd7ae94b3b87ff269e3248
ahyej2 <ahyej2@naver.com>
2021-05-25 17:05:11 +0900

    해결

--- Groupware/src/main/java/com/spring/groupware/insa/model/InsaDAO.java
+++ Groupware/src/main/java/com/spring/groupware/insa/model/InsaDAO.java
@@ -3,14 +3,24 @@ package com.spring.groupware.insa.model;
 import javax.annotation.Resource;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.spring.groupware.member.model.MemberVO;
-
+@Component
+@Repository
 public class InsaDAO implements InterInsaDAO {
 
 
-	  @Resource
-	  private SqlSessionTemplate sqlsession; 
+
+	@Resource
+	private SqlSessionTemplate sqlsession; // 로컬 DB에 연결
+
+	@Resource
+	private SqlSessionTemplate sqlsession2; // 원격 DB에 연결
+	
+	@Resource
+	private SqlSessionTemplate sqlsession3; // 로컬 DB에 hr로 연결
 	
 	// 인사정보 등록하기
 	@Override
