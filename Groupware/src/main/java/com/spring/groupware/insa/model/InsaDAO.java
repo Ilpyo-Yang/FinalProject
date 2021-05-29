b2e218a1a39c04b63766462df873266da7a58c5e
ahyej2 <ahyej2@naver.com>
2021-05-29 22:22:49 +0900

    자격증 학력정보 추가

--- Groupware/src/main/java/com/spring/groupware/insa/model/InsaDAO.java
+++ Groupware/src/main/java/com/spring/groupware/insa/model/InsaDAO.java
@@ -11,38 +11,61 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-import com.spring.groupware.member.model.MemberVO;
+import com.spring.groupware.insa.model.InsaVO;
 @Component
 @Repository
 public class InsaDAO implements InterInsaDAO {
 
-
-
 	@Resource
 	private SqlSessionTemplate sqlsession; // 로컬 DB에 연결
-
-	@Resource
-	private SqlSessionTemplate sqlsession2; // 원격 DB에 연결
-	
-	@Resource
-	private SqlSessionTemplate sqlsession3; // 로컬 DB에 hr로 연결
 	
 	// 인사정보 등록하기
 	@Override
-	public int insaRegister1End(MemberVO membervo) {
-		int n = sqlsession.insert("insa.insaRegisterEnd", membervo);
-		System.out.println("mem=>"+membervo.getMbr_id());
+	public int insaRegister1End(InsaVO insavo, int seq) {
+		int n=0;
+		insavo.setMbr_seq(seq);
+		try {
+			n = sqlsession.insert("insa.insaRegisterEnd", insavo);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("dao => "+n);
+	//	System.out.println("mem=>"+insavo.getMbr_id());
 		return n;
 	}
 
 
+
 	// 멤버정보 불러오기
 	@Override
-	public List<MemberVO> getMemberList(String category) {
+	public List<InsaVO> getInsaList(String category) {
 		Map<String,String> paraMap = new HashMap<>();
 		paraMap.put("category", category);
-		List<MemberVO> memberList= sqlsession.selectList("insa.memberList",paraMap);
-		return memberList;
+		List<InsaVO> insaList= sqlsession.selectList("insa.insaList",paraMap);
+		return insaList;
 	}
+
+
+	@Override
+	public int getSequence() {
+		int seq = Integer.parseInt(sqlsession.selectOne("insa.getInsaSequence"));
+		return seq;
+	}
+
+
+	// view1할 멤버 정보 가져오기
+	@Override
+	public InsaVO getInsaView1(String seq) {
+		InsaVO insavo = sqlsession.selectOne("insa.getInsaView1", seq);
+		return insavo;
+	}
+
+	// view2할 멤버 정보 가져오기@Override
+	public List<InsaVO> getEduList(String seq) {
+		List<InsaVO> eduList = sqlsession.selectList("insa.getEduList", seq);
+		return eduList;
+	}
+
 
 }
--- Groupware/src/main/java/com/spring/groupware/insa/service/InsaService.java
+++ Groupware/src/main/java/com/spring/groupware/insa/service/InsaService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 import com.spring.groupware.insa.model.InsaDAO;
 import com.spring.groupware.insa.model.InterInsaDAO;
-import com.spring.groupware.member.model.MemberVO;
+import com.spring.groupware.insa.model.InsaVO;
 
 
 //=== #31. Service 선언 === 
@@ -27,26 +27,47 @@ public class InsaService implements InterInsaService {
 
 	
 	// 인사정보 등록하기
-	
 	@Override
-	public int insaRegister1End(MemberVO membervo) {
-		int n = idao.insaRegister1End(membervo);
+	public int insaRegister1End(InsaVO Insavo, int seq) {
+		int n = idao.insaRegister1End(Insavo, seq);
+		System.out.println("service => "+n);
 		return n;
 	}
 
 
+
 	@Override
-	public List<MemberVO> getMemberList(String category) {
-		List<MemberVO> memberList = idao.getMemberList(category);
-		return memberList;
+	public List<InsaVO> getInsaList(String category) {
+		List<InsaVO> InsaList = idao.getInsaList(category);
+		return InsaList;
 	}
 
-	
-	
 
-	
-	
-	
+	// 시퀀스 가져오기
+	@Override
+	public int getSequence() {
+		int seq = idao.getSequence();
+		return seq;
+	}
+
+
+
+	// view1할 멤버 정보 가져오기
+	@Override
+	public InsaVO getInsaView1(String seq) {
+		InsaVO Insavo = idao.getInsaView1(seq);
+		return Insavo;
+	}
+
+
+
+	// view2할 멤버 정보 가져오기
+	@Override
+	public List<InsaVO> getEduList(String seq) {
+		List<InsaVO> eduList = idao.getEduList(seq);
+		return eduList;
+	}
+
 	
 	
 }
